212ff5a34a51ce8ab5a27794dd1f7c7e20dcdcd2
utkarshk014 <utkarsh@instasell.in>
2025-08-22 16:43:59 +0530

    frontend shadcn and dashboard set up

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import * as React from "react";
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Presentation,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react";
+
+import { NavProjects } from "@/components/nav-projects";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  projects: [
+    {
+      name: "Projects",
+      url: "#",
+      icon: Presentation,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <h1>Logo</h1>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
